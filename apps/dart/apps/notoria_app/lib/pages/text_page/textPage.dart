69e0aa1d54068dd3fcd590845956c71f03d3b9ee
koudaihirata <2230051@ecc.ac.jp>
2024-10-27 11:43:44 +0900

    textページ作成

---
+++ apps/dart/apps/notoria_app/lib/pages/text_page/textPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class TextPage extends StatelessWidget {
+  const TextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6F4EC),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+
+          ],
+        )
+      ),
+    );
+  }
+}
